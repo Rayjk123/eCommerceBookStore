ac9f3a7d3fb276c283b27f7daebffc0e5b17552d
Panda <bdleecs95@gmail.com>
2018-06-21 19:57:18 -0400

    Updated DB Access
    Updated to have a separate update and delete method.
    Also added in some comments for the code as well

--- src/persist_layer/DbAccess.java
+++ src/persist_layer/DbAccess.java
@@ -18,6 +18,7 @@ public class DbAccess {
 	
 	public static Connection connect() {
 		Connection con = null;
+		
 		try {
 			Class.forName(DRIVE_NAME);
 			String jdbcUrl = "jdbc:mysql://" + HOST_NAME + ":" + "3306" + "/" + "bookStore" + "?user=" + "root" + "&password=" + "emanSaleh17";
@@ -30,49 +31,67 @@ public class DbAccess {
 			e.printStackTrace();
 		}
 		return con;
-	} // end of connect
+	}
 	
 	//If rows = 0 after the code is ran, that means 0 rows were updated.
 	public static int insert (String query){
 		int rows = 0;
+		
 		try {
 			Connection con = connect();
 			java.sql.PreparedStatement stmt = con.prepareStatement(query);
 			rows = stmt.executeUpdate();
 			disconnect(con);
+			
 			return rows;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return rows;
 	}
 	
-	//Use update when you want to remove a row or change data
+	public static int delete(String query){
+		return updateAndDelete(query);
+	}
+	
 	public static int update(String query){
+		return updateAndDelete(query);
+	}
+	
+	// Logic to update or delete row
+	private static int updateAndDelete(String query) {
 		int rows=0;
+		
 		try{
 			Connection con = connect();
 			java.sql.PreparedStatement stmt = con.prepareStatement(query);
 			rows=stmt.executeUpdate();
 			disconnect(con);
+			
 			return rows;
 		}catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return rows;
 	}
 	
+	// select query stuff
 	public static ResultSet retrieve (Connection con, String query) {
 		ResultSet rset = null;
+		
 		try {
 			Statement stmt = con.createStatement();
 			rset = stmt.executeQuery(query);
+			
 			return rset;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return rset;
-	}// end of retrieve
+	}
 	
 	public static void disconnect(Connection con) {
 		try {
@@ -82,5 +101,5 @@ public class DbAccess {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	} // end of closeConnection
+	}
 }
