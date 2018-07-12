82c8f5ad5b66fdfa8330d6409db5b34828f84ce7
stfra <stfra@Tyler-Acer.mshome.net>
2018-07-12 19:08:25 -0400

    created adminBar for admin view

---
+++ WebContent/js/adminBar.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var headerHtml = "<!--Nav Bar-->\n" +
+        "        <ul class=\"navbar-nav mr-auto\">\n" +
+        "            <li class=\"nav-item active\">\n" +
+        "                <a class=\"nav-link\">Admin Tools: <span class=\"sr-only\">(current)</span></a>\n" +
+        "            </li>\n" +
+        "            <li class=\"nav-item active\">\n" +
+        "                <a class=\"nav-link\" href=\"#\">Manage Inventory  <span class=\"sr-only\">(current)</span></a>\n" +
+        "            </li>\n" +
+        "            <li class=\"nav-item active\">\n" +
+        "                <a class=\"nav-link\" href=\"#\">Manage Accounts  <span class=\"sr-only\">(current)</span></a>\n" +
+        "            </li>\n" +
+        "            <li class=\"nav-item active\">\n" +
+        "                <a class=\"nav-link\" href=\"#\">Manage Orders  <span class=\"sr-only\">(current)</span></a>\n" +
+        "            </li>\n" +
+        "</nav>";
+    
+   $("#adminBar").append(headerHtml);
+});
