959dc101e215af3bdcbc08cb8851b128e06bd10f
stfra <stfra@Tyler-Acer.mshome.net>
2018-07-14 14:04:22 -0400

    report class shell

---
+++ src/domain_layer/Report.java
@@ -0,0 +1,27 @@
+package domain_layer;
+
+public class Report {
+	
+	public Report() {
+		
+	}
+	
+	public void getOwnerReport(){
+		//Year to date sales
+		//others, but undefined for now
+	}
+	
+	public void getEndOfDayReport() {
+		//sales for today only
+		//whole store
+	}
+	
+	public void getPublisherReport() {
+		//sales for publisher's or vendor's books
+	}
+	
+	public void getLowInventoryReport() {
+		//low inventory for whole store
+		//low inventory for publisher
+	}
+}
