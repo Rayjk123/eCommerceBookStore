cafa4150206a6ce87014a06366925e3b1505ef0b
stfra <stfra@Tyler-Acer.mshome.net>
2018-07-15 14:24:03 -0400

    addCart and removeFromCart logic

--- src/domain_layer/Cart.java
+++ src/domain_layer/Cart.java
@@ -1,4 +1,5 @@
 package domain_layer;
+import java.util.Arrays;
 
 public class Cart {
 	private Book[] contents;
@@ -15,13 +16,29 @@ public class Cart {
 		this.contents = contents;
 	}
 	
-	public void addToCart(Book book) {
+	public Book[] addToCart(Book addedBook) {
 		//array arithmetic to add book to array
+		Book[] tempCart = new Book[getContents().length+1];
 		
-		//use temp array method
+		System.arraycopy(getContents(),  0, tempCart, 0, getContents().length);
+		
+		tempCart[getContents().length+1] = addedBook;
+		
+		setContents(tempCart);
+		
+		return tempCart; //contents after adding to the array
 	}
 	
 	public void removeFromCart(Book book) {
+		Book[] tempCart = new Book[getContents().length-1];
 		
+		for (int i = 1; i < getContents().length; i++){
+			int j = 1;
+			if (getContents()[i] != book)
+			{
+				tempCart[j] = getContents()[i];
+				j++;
+			}
+		}
 	}
 }
