2c93e0eae9d01f7d375a303a7da131bcb68cdaf3
stfra <stfra@Tyler-Acer.mshome.net>
2018-07-17 12:25:51 -0400

    refactor

--- src/boundary/EditCart.java
+++ src/boundary/EditCart.java
@@ -18,19 +18,19 @@ import domain_layer.Book;
 import domain_layer.Customer;
 
 @WebServlet("/AddToCart")
-public class AddToCart extends HttpServlet {
+public class EditCart extends HttpServlet {
 
 	/**
      * @see HttpServlet#HttpServlet()
      */
-	public AddToCart(){
+	public EditCart(){
 		super(); //HttpServlet constructor
 	}
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
 		
 		try {
-			addToCart(request, response);
+			EditCart(request, response, session);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,18 +45,9 @@ public class AddToCart extends HttpServlet {
 			doGet(request, response);
 	}
 	
-	private void addToCart(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-		HttpSession session = request.getSession();
-		Customer customer = (Customer)session.getAttribute("customer"); //TODO need create a customer object at login and setAttribute
-		Book book = new Book(Integer.parseInt(request.getParameter("isbn")));
+	private void EditCart(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws SQLException {
+		Customer customer = (Customer)session.getAttribute("customer");
 		
-		book.setStock(Integer.parseInt(request.getParameter("stock")));
-		int quantity = Integer.parseInt(request.getParameter("quantity")); //quantity the user wants to add to cart 
-																		//TODO need a default quantity of 1 in bookDetail.html
-																		//TODO edit cart will have a field to set quantity in cart
-		//can't add more than what's in stock to cart
-		if (book.getStock() - quantity > 0) {
-			Query.addToCart(customer, book, quantity); 
-		}
+		
 	}
 }
